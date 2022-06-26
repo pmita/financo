e156c93892c7006c3092ba24837d0eb9ac9dc530
pmita <panos.mita2@outlook.com>
2022-06-26 17:59:01 +0100

    logging the user out with our useLogOut hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,25 @@
 import React from 'react';
 //ROUTER
 import { Link } from 'react-router-dom';
+//HOOKS
+import { useLogOut } from '../hooks/useLogOut';
+//STYLES
 import styles from './Navbar.module.css';
 
+
 const Navbar = () => {
+    //STATE & VARIABLES
+    const { logout } = useLogOut(); 
+
     return(
         <nav className={styles.navbar}>
             <ul>
                 <li className={styles.title}>myMoney</li>
                 <li><Link to='/login'>Login</Link></li>
                 <li><Link to='/signup'>Sign Up</Link></li>
-                
+                <li>
+                    <button className='btn' onClick={logout}>Log Out</button>
+                </li>
             </ul>
         </nav>
     );
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -20,6 +20,7 @@ export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     });
+    console.log('AuthContext state: ', state);
 
     return(
         <AuthContext.Provider value={{ ...state, dispatch }}>
